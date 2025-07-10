fed798a29fe8c10d1fa451e33ab3d5e00a0193e8
Tiago Dall'Igna <tiago@dalligna.com>
2025-07-10 11:27:31 +0200

    virtual keyboard without modifier keys

---
+++ code_files/cs.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Linq;
+using xxx;
+
+namespace dis_test;
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        var docid = "14724c8a0n8anwpw98nawlwiuhwL*$rwy8";
+        for(int i=0; i<2; i++)
+        {
+            var asd = new DisClient(asd, qwe);
+            asd.value = "new value";
+
+            Console.Write(asd.ToString());
+        }
+    }
+}
--- src/main.go
+++ src/main.go
@@ -1,9 +1,13 @@
+//go:build !go1.24.4
+
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 	"text/template"
 )
 
@@ -15,11 +19,41 @@ func main() {
 			http.Error(w, "Not found", http.StatusNotFound)
 			return
 		}
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		RenderGame("cpp.cpp", w)
+	})
 
-		tmpl, _ := template.ParseFiles("index.gohtml")
-		tmpl.Execute(w, nil)
+	http.HandleFunc("/{code}", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		code := r.PathValue("code")
+		RenderGame(code, w)
 	})
 
 	log.Println("Server starting on " + os.Getenv("APP_PORT"))
 	log.Fatal(http.ListenAndServe(os.Getenv("APP_PORT"), nil))
+}
+
+// func enableCors(w *http.ResponseWriter) {
+// 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+// }
+
+func RenderGame(code string, w http.ResponseWriter) {
+
+	code_ext := filepath.Ext(code)
+	data_bytes, _ := os.ReadFile("../code_files/" + code)
+	data := map[string]any{
+		"code":        fmt.Sprintf("%s", data_bytes),
+		"import_lang": GetImportLang(code_ext),
+		"lang_code":   code_ext,
+	}
+	tmpl, _ := template.ParseFiles("./index.gohtml")
+	tmpl.Execute(w, data)
+}
+func GetImportLang(ext string) string {
+	switch ext {
+	case ".cs":
+		return "csharp"
+	default:
+		return ext[1:]
+	}
 }
